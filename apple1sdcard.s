2193f6f6a46bc9ce693b0dba581549170809c69e
nino-porcino <nino.porcino@gmail.com>
2022-04-04 09:54:35 +0200

    align with SD card firmware

--- apple1sdcard.s
+++ apple1sdcard.s
@@ -9,10 +9,9 @@
 
 SDCARD_FILENAME := $0200    ; SD card buffer for filename (label "filename" from sdcard.sym)
 SDCARD_MONITOR  := $8000    ; EPROM entry point for SD card OS
-SDCARD_LOAD     := $8d0e    ; label "comando_load_bas" from sdcard.sym 
+SDCARD_LOAD     := $8d11    ; label "comando_load_bas" from sdcard.sym
 SDCARD_SAVE     := $8a75    ; label "comando_asave" from sdcard.sym
-SDCARD_DIR      := $90de    ; label "comando_dir" from sdcard.sym 
-SDCARD_EPROM_ID := $95e3    ; label "chksum_table" from sdcard.sym
+SDCARD_EPROM_ID := $95e9    ; label "chksum_table" from sdcard.sym
 
 ; ----------------------------------------------------------------------------
 ; See if Apple-1 SD card is present and display error if not
