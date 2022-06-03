0b8338ccecc8a4688c6f86bcc876861e901d834d
nino-porcino <nino.porcino@gmail.com>
2022-06-03 14:34:02 +0200

    upgrade to SD OS 1.2

--- apple1sdcard.s
+++ apple1sdcard.s
@@ -8,10 +8,18 @@
 .export SDCardLoad, SDCardSave, SDCardMenu
 
 SDCARD_FILENAME := $0200    ; SD card buffer for filename (label "filename" from sdcard.sym)
+
+; values for SD OS 1.1 firmware EPROM
+; SDCARD_MONITOR  := $8000    ; EPROM entry point for SD card OS
+; SDCARD_LOAD     := $8d11    ; label "comando_load_bas" from sdcard.sym
+; SDCARD_SAVE     := $8a75    ; label "comando_asave" from sdcard.sym
+; SDCARD_EPROM_ID := $95e9    ; label "chksum_table" from sdcard.sym
+
+; values for SD OS 1.2 firmware EPROM
 SDCARD_MONITOR  := $8000    ; EPROM entry point for SD card OS
-SDCARD_LOAD     := $8d11    ; label "comando_load_bas" from sdcard.sym
-SDCARD_SAVE     := $8a75    ; label "comando_asave" from sdcard.sym
-SDCARD_EPROM_ID := $95e9    ; label "chksum_table" from sdcard.sym
+SDCARD_LOAD     := $8d1a    ; label "comando_load_bas" from sdcard.sym
+SDCARD_SAVE     := $8a7e    ; label "comando_asave" from sdcard.sym
+SDCARD_EPROM_ID := $95f2    ; label "chksum_table" from sdcard.sym
 
 ; ----------------------------------------------------------------------------
 ; See if Apple-1 SD card is present and display error if not
---
+++ t.bat
@@ -0,0 +1,3 @@
+del *.o & del *.bin
+make
+copy applesoft-lite.bin ..\apple1-emu\software\sdcard_image\ASOFT\applesoft-sd12#066000 /y
