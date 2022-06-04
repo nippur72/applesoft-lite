e56ff81e742885997a3572ed5c82d113d9ce3e35
nino-porcino <nino.porcino@gmail.com>
2022-06-04 10:21:49 +0200

    rename build script file name

---
+++ m.bat
@@ -0,0 +1,3 @@
+del *.o & del *.bin
+make
+copy applesoft-lite.bin ..\apple1-emu\software\sdcard_image\ASOFT\APPLESOFT-SD#066000 /y
--- t.bat
+++
@@ -1,3 +0,0 @@
-del *.o & del *.bin
-make
-copy applesoft-lite.bin ..\apple1-emu\software\sdcard_image\ASOFT\applesoft-sd12#066000 /y
